743fe5c0c76180a41301f418aadbdd3b0c1b07a2
unknown <andrejteryukhov5@gmail.com>
2023-10-24 17:45:54 +0300

    added some the functionality

--- src/main/java/CertificateInfo.java
+++ src/main/java/CertificateInfo.java
@@ -1,4 +1,7 @@
+import io.javalin.Javalin;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.apache.commons.net.util.SubnetUtils;
 
@@ -9,11 +12,32 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class CertificateInfo extends Thread{
+    private List<String> responseList;
 
-    public static void main(String[] args) throws Exception {
-        SubnetUtils utils = new SubnetUtils("140.82.121.0/24");
+    public static void addRoutes(Javalin app) {
+        app.get("/IP/{address}/{mask}", ctx -> {
+            calculation(ctx.pathParam("address") + "/" +  ctx.pathParam("mask"));
+            ctx.result("Successfully processing input addresses: " + ctx.pathParam("address") + "/" +  ctx.pathParam("mask")
+            + ". Check /result to see all domains");
+        });
+
+        app.get("/result", ctx -> {
+            ctx.result("Response List: \n" + Files.readString(Path.of(".\\toResult.txt")));
+        });
+    }
+
+    public static void calculation(String IPWithSubnet) throws IOException {
+        SubnetUtils utils = new SubnetUtils(IPWithSubnet);
         String[] allIps = utils.getInfo().getAllAddresses();
 
         Path pathToCheck = Path.of(".\\toResult.txt");
@@ -33,6 +57,27 @@ public class CertificateInfo extends Thread{
         } catch (InterruptedException e) {
             System.out.println("Interrupted");
         }
+
+        BufferedReader reader = new BufferedReader(new FileReader(pathToCheck.toFile()));
+        CertificateInfo cert = new CertificateInfo();
+
+        List<String> proxy = new ArrayList<>();
+        String line = reader.readLine();
+        while (line != null){
+            line = reader.readLine();
+            if (line == null){
+                break;
+            }
+            proxy.add(line);
+        }
+        cert.setResponseList(proxy);
+
+        reader.close();
+    }
+
+    public static void main(String[] args){
+        Javalin app = Javalin.create().start(8081);
+        addRoutes(app);
     }
 
 
@@ -55,29 +100,28 @@ public class CertificateInfo extends Thread{
             }
         }
 
-        public static void certInformation(String aURL) throws Exception{
+        public static void certInformation(String aURL) throws Exception {
             URL destinationURL = new URL(aURL);
             HttpsURLConnection conn = (HttpsURLConnection) destinationURL.openConnection();
             try {
                 conn.connect();
-            }catch (ConnectException e){
+            } catch (ConnectException e) {
                 return;
             }
 
-            if (!(conn.getResponseCode() == HttpURLConnection.HTTP_OK)){
+            if (!(conn.getResponseCode() == HttpURLConnection.HTTP_OK)) {
                 return;
-            }else {
+            } else {
                 Certificate[] certs = conn.getServerCertificates();
 
                 Path pathToFile = Path.of(".\\temp.txt");
 
-                if (Files.exists(pathToFile)){
+                if (Files.exists(pathToFile)) {
                     Files.delete(pathToFile);
                 }
-                BufferedWriter writer = new BufferedWriter(new FileWriter(pathToFile.toFile(),true));
+                BufferedWriter writer = new BufferedWriter(new FileWriter(pathToFile.toFile(), true));
                 for (Certificate cert : certs) {
-                    //System.out.println("Certificate is: " + cert);
-                    if(cert instanceof X509Certificate) {
+                    if (cert instanceof X509Certificate) {
                         X509Certificate x = (X509Certificate) cert;
                         writer.write(String.valueOf(x));
                     }
@@ -89,19 +133,37 @@ public class CertificateInfo extends Thread{
                 BufferedReader reader = new BufferedReader(new FileReader(pathToFile.toFile()));
 
                 String line = reader.readLine();
-                while (line != null){
+                while (line != null) {
                     line = reader.readLine();
-                    if (line == null){
+                    if (line == null) {
                         break;
                     }
-                    if (line.matches(".*DNSName:.*")){
+                    if (line.matches(".*DNSName:.*")) {
                         toResult.write(line.trim() + "\n");
                     }
                 }
                 toResult.close();
                 reader.close();
                 Files.delete(pathToFile);
+
+                removeRepetitions(toRes);
             }
+        }
+
+        public static void removeRepetitions(Path path) throws IOException {
+            BufferedReader reader = new BufferedReader(new FileReader(path.toFile()));
+            Set<String> lines = new HashSet<>(10000); // maybe should be bigger
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+            reader.close();
+            BufferedWriter writer = new BufferedWriter(new FileWriter(path.toFile()));
+            for (String unique : lines) {
+                writer.write(unique);
+                writer.newLine();
+            }
+            writer.close();
         }
     }
 }
